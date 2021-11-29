c3ba5edfb22681c05527774bb22988901839c737
Code Sandbox <hello@codesandbox.io>
2021-11-29 11:20:41 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.c9b081d024af2de9f25230cde398c125.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55b4415e10c79fa6fc93c7587ce53d59",
+    "url": "https://nmn1998.github.io/csb-b1qjw/index.html"
+  },
+  {
+    "revision": "0fd7d30f77bc6c408602",
+    "url": "https://nmn1998.github.io/csb-b1qjw/static/js/2.8ee70edf.chunk.js"
+  },
+  {
+    "revision": "ae453fee75afe198e285",
+    "url": "https://nmn1998.github.io/csb-b1qjw/static/js/main.a8f62cce.chunk.js"
+  },
+  {
+    "revision": "8b2788bc93bd4f930ae8",
+    "url": "https://nmn1998.github.io/csb-b1qjw/static/js/runtime-main.134322b4.js"
+  }
+]);
